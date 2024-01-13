903847c5b3fc88ef52a741abf6faa22011f58207
josill <josill@ttu.ee>
2024-01-14 01:40:58 +0200

    Moving sphere

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      <script src="https://cdn.jsdelivr.net/npm/three@0.143/build/three.min.js"></script>
+    </Html>
+  )
+}
