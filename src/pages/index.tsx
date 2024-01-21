d751ca6994db9de98d7a02bed159e3c354479b2d
josill <josill@ttu.ee>
2024-01-21 20:23:15 +0200

    Revert "css bug fix"
    This reverts commit 109caccf39b577f10eafdf90c96c58c0b07b8c87.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,151 @@
-import React from "react";
-import Hero from "../sections/Hero";
+import React, { useEffect, useState } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { vertexShaderGLSL, fragmentShaderGLSL } from "../shaders";
+import { useTheme } from "next-themes";
 
-export default function Index() {
+const SphereAnimation = () => {
+  const { theme } = useTheme();
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 800,
+    height: 600,
+  });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.setZ(60);
+
+    const light = new THREE.SpotLight(0xffffff, 100);
+    light.castShadow = true;
+    light.position.set(0, 0, 0);
+    const uniforms = {
+      u_time: { type: "f", value: 0.0 },
+      u_resolution: {
+        type: "v2",
+        value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+      },
+      u_lightColor: { type: "c", value: new THREE.Color(0xffffff) },
+      u_lightPosition: { type: "v3", value: light.position },
+      u_ambientLight: { type: "c", value: new THREE.Color(0x000000) },
+      u_mousePosition: { type: "v2", value: new THREE.Vector2(0, 0) },
+      u_cameraPosition: { type: "v3", value: camera.position },
+    };
+    const material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      wireframe: true,
+      vertexShader: vertexShaderGLSL,
+      fragmentShader: fragmentShaderGLSL,
+    });
+
+    const geometry = new THREE.IcosahedronGeometry(
+      16,
+      theme === "dark" ? 60 : 20
+    );
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+
+    scene.add(light);
+    scene.add(sphere);
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#bg") as HTMLCanvasElement,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    const width = window.innerWidth;
+    const header = document.querySelector("nav") || { clientHeight: 0 };
+    const titleBlock = document.getElementById("title-block") || { clientHeight: 0 };
+    const height = window.innerHeight - header.clientHeight - titleBlock.clientHeight;
+
+    renderer.setSize(width, height);
+    const clearColor = theme === "dark" ? 0x000000 : 0xffffff;
+    renderer.setClearColor(clearColor);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.enableRotate = false;
+    controls.dampingFactor = 0.25;
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.enableZoom = false;
+
+    const handleResize = () => {
+      if (typeof window !== "undefined") {
+        setWindowDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const header = document.querySelector("nav") || { clientHeight: 0 };
+        const titleBlock = document.getElementById("title-block") || { clientHeight: 0 };
+        const height = canvas.clientHeight - header.clientHeight - titleBlock.clientHeight;
+
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+
+        return needResize;
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    function animate() {
+      uniforms.u_time.value += 0.003;
+      const rotationSpeed = 0.002;
+
+      // Update sphere rotation
+      sphere.rotation.x += rotationSpeed;
+      sphere.rotation.y += rotationSpeed;
+
+      if (handleResize()) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+      }
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    const handleMouseMove = (event: any) => {
+      const x = (event.clientX / window.innerWidth) * 2 - 1;
+      const y = -(event.clientY / window.innerHeight) * 2 + 1;
+      setMousePosition({ x, y });
+
+      uniforms.u_mousePosition.value.set(x, y);
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [theme, windowDimensions]);
 
   return (
-    <div className="flex flex-col items-center justify-center font-beVietnam bg-bgWhite dark:bg-black">
-      <Hero />
+    <div className="flex flex-col items-center justify-center overflow-hidden font-beVietnam bg-bgWhite dark:bg-black">
+      <div className="flex flex-col justify-center items-center px-4" id="title-block">
+        <h2 className="text-[20px] text-darkGray mt-8 mb-4">Empowering tomorrow</h2>
+        <h1 className="text-[30px] text-lightGray text-center px-4">Innovative <span className="text-lightBlue dark:text-darkBlue">IT Solutions</span> for a connected world</h1>
+      </div>  
+      <canvas id="bg" />
+      {/* <h1 className="font-spotnik absolute text-[90px] text-gray-900">Attention</h1> */}
     </div>
   );
 };
+
+export default SphereAnimation;
