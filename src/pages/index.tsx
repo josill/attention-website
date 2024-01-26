50a2641c21439d4b488d23594849aef8191cd8e2
josill <jonathan.sillak2@gmail.com>
2024-01-26 16:31:21 +0200

    why-us hover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -490,99 +490,99 @@ export default function Index({ sidebarOpen, setSidebarOpen }: { sidebarOpen: bo
       </section> */}
       <Team />
       {/* <section
-      className="flex flex-col justify-center items-center mt-[60px] font-beVietnam"
-      id="why-us"
-    >
-      <div className="flex flex-col justify-center items-center w-[100vw]">
-        <h2 className="text-customGray dark:text-white text-[25px] font-light">
-          Why us
-        </h2>
-        <div className="w-full flex justify-evenly text-black">
-          <div className="w-full flex flex-row">
-            <div className="min-w-[10px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-            <div className="bg-[url('/why-us/scratched-bg-light.svg')] dark:bg-[url('/why-us/scratched-bg-dark.svg')] min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-            <div className="flex-grow"></div>
-          </div>
-        </div>
-        {whyUsContent.map((item, index) => {
-          return (
-            <div className="w-full flex flex-row border-t-[0.5px] border-lightGray2 dark:border-darkGray3 group">
-              <div className="min-w-[10px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-              <div className="min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-              <div
-                className={`flex flex-col justify-center items-start px-4 py-8 gap-y-[10px] flex-grow text-darkBlue4 dark:text-[#DBDBDB] rounded-[30px] hover:border-3 hover:border-darkBlue4
-                ${
-                  resolvedTheme !== "light" &&
-                  "hover:bg-[#272B34] hover:border-[#4A648B]"
-                }`}
-              >
-                <img
-                  src={`${
-                    resolvedTheme === "light"
-                      ? item.iconPathLight
-                      : item.iconPathDark
-                  }`}
-                  alt={item.header}
-                  height={20}
-                  width={20}
-                />
-                <h3 className="text-[20px]">{item.header}</h3>
-                <p className="text-[14px]">{item.description}</p>
-              </div>
-              <div
-                className={`bg-[url('/why-us/scratched-bg-tall-light.svg')] min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3
-                ${
-                  resolvedTheme !== "light" &&
-                  "dark:bg-[url('/why-us/scratched-bg-tall-dark.svg')]"
-                }`}
-              ></div>
-              <div className="min-w-[20px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-            </div>
-          );
-        })}
-        <div className="w-full flex flex-row border-y-[0.5px] border-lightGray2 dark:border-darkGray3 group">
+    className="flex flex-col justify-center items-center mt-[60px] font-beVietnam"
+    id="why-us"
+  >
+    <div className="flex flex-col justify-center items-center w-[100vw]">
+      <h2 className="text-customGray dark:text-white text-[25px] font-light">
+        Why us
+      </h2>
+      <div className="w-full flex justify-evenly text-black">
+        <div className="w-full flex flex-row">
           <div className="min-w-[10px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-          <div className="min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
-          <div
-            className={`relative flex flex-col justify-center items-center px-8 py-8 gap-y-[20px] flex-grow text-center text-darkBlue4 dark:text-[#DBDBDB] rounded-[30px] hover:border-3 hover:border-darkBlue4
-                ${
-                  resolvedTheme !== "light" &&
-                  "hover:bg-[#272B34] hover:border-[#4A648B]"
-                }`}
-          >
-            <img
-              src={`${
-                resolvedTheme === "light"
-                  ? "/why-us/mail-icon-light.svg"
-                  : "/why-us/mail-icon-dark.svg"
-              }`}
-              alt="mail icon"
-              height={45}
-              width={45}
-            />
-            <h3 className="text-[20px] font-semibold">Want to know more?</h3>
-            <img className="contact-arrow animate-pulse"
-              src={`${
-                resolvedTheme === "light"
-                  ? "/why-us/arrow-icon-light.svg"
-                  : "/why-us/arrow-icon-dark.svg"
-              }`}
-              alt="mail icon"
-              height={90}
-              width={90}
-            />
-          </div>
-          <div
-            className={`bg-[url('/why-us/scratched-bg-tall-light.svg')] min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3
-                ${
-                  resolvedTheme !== "light" &&
-                  "dark:bg-[url('/why-us/scratched-bg-tall-dark.svg')]"
-                }`}
-          ></div>
-          <div className="min-w-[20px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+          <div className="bg-[url('/why-us/scratched-bg-light.svg')] dark:bg-[url('/why-us/scratched-bg-dark.svg')] min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+          <div className="flex-grow"></div>
         </div>
       </div>
-    </section> */}
+      {whyUsContent.map((item, index) => {
+        return (
+          <div
+            className="w-full flex flex-row border-t-[0.5px] border-lightGray2 dark:border-darkGray3 group"
+            key={index}
+          >
+            <div className="min-w-[10px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+            <div className="min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+            <div
+              className={`flex flex-col justify-center items-start px-4 py-8 gap-y-[10px] flex-grow text-darkBlue4 dark:text-[#DBDBDB] rounded-[30px] group-hover:border-3 group-hover:border-darkBlue4
+              ${
+                resolvedTheme === "dark" &&
+                "group-hover:bg-[#272B34] group-hover:border-[#4A648B]"
+              }`}
+            >
+              <img
+                src={`${
+                  resolvedTheme === "dark"
+                    ? item.iconPathDark
+                    : item.iconPathLight
+                }`}
+                alt={item.header}
+                height={20}
+                width={20}
+              />
+              <h3 className="text-[20px]">{item.header}</h3>
+              <p className="text-[14px]">{item.description}</p>
+            </div>
+            <div
+              className={`group-hover:bg-[url('/why-us/scratched-bg-tall-light.svg')] min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3
+              group-hover:text-red group-focus:text-red
+              ${
+                resolvedTheme === "dark" &&
+                "group-hover:dark:bg-[url('/why-us/scratched-bg-tall-dark.svg')]"
+              }`}
+            >
+            </div>
+            <div className="min-w-[20px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+          </div>
+        );
+      })}
+      <div className="w-full flex flex-row border-y-[0.5px] border-lightGray2 dark:border-darkGray3 group">
+        <div className="min-w-[10px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+        <div className="min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+        <div
+          className="relative flex flex-col justify-center items-center px-8 py-8 gap-y-[20px] flex-grow text-darkBlue4 dark:text-[#DBDBDB] rounded-[30px] group-hover:hover:border-3 group-hover:border-darkBlue4        
+          group-hover:dark:bg-[#272B34] group-hover:border-[#4A648B]"
+        >
+          <img
+            src={`${
+              resolvedTheme === "dark"
+                ? "why-us/mail-icon-dark.svg"
+                : "why-us/mail-icon-light.svg"
+            }`}
+            alt="mail icon"
+            height={45}
+            width={45}
+          />
+          <h3 className="text-[20px] font-semibold">Want to know more?</h3>
+          <img
+            className="contact-arrow animate-pulse"
+            src={`${
+              resolvedTheme === "light"
+                ? "why-us/arrow-icon-dark.svg"
+                : "why-us/arrow-icon-light.svg"
+            }`}
+            alt="mail icon"
+            height={90}
+            width={90}
+          />
+        </div>
+        <div
+          className="group-hover:bg-[url('/why-us/scratched-bg-tall-light.svg')] min-h-[60px] min-w-[60px] border-x-[0.5px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3
+          group-hover:dark:bg-[url('/why-us/scratched-bg-tall-dark.svg')]"
+        ></div>
+        <div className="min-w-[20px] border-t-[0.5px] border-lightGray2 dark:border-darkGray3"></div>
+      </div>
+    </div>
+  </section> */}
       <WhyUs content={whyUsContent} />
       {/* <section id="contact" className="mt-32">
       <div className="flex flex-col justify-center items-center gap-y-4 px-8 text-textBlue">
